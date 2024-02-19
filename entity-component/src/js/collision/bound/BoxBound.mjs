1221f367e47d2af01739801cdf950fb18515f3fa
Jempasam <samueldemont83@gmail.com>
2024-02-19 11:18:28 +0100

    menu du index principal

--- entity-component/src/js/collision/bound/BoxBound.mjs
+++ entity-component/src/js/collision/bound/BoxBound.mjs
@@ -8,13 +8,13 @@ export class BoxBound extends Bound{
 
     /**
      * @inheritdoc
-     * @param {[number,number,number]} direction
+     * @param {[number,number,number]} point
      * @returns {[number,number,number]}
      */
-    getNearestPoint(direction){
-        let ox=Math.abs(direction[0]);
-        let oy=Math.abs(direction[1]);
-        let oz=Math.abs(direction[2]);
+    getNearestPoint(point){
+        let ox=Math.abs(point[0]);
+        let oy=Math.abs(point[1]);
+        let oz=Math.abs(point[2]);
         let multiplier
         if(ox>oy){
             if(ox>oz){
@@ -30,30 +30,30 @@ export class BoxBound extends Bound{
                 multiplier=1/oz;
             }
         }
-        return [direction[0]*multiplier, direction[1]*multiplier, direction[2]*multiplier];
+        return [point[0]*multiplier, point[1]*multiplier, point[2]*multiplier];
     }
 
     /**
      * @inheritdoc
-     * @param {[number,number,number]} direction
+     * @param {[number,number,number]} point
      * @returns {[number,number,number]}
      */
-    getNormal(direction){
-        let ox=Math.abs(direction[0]);
-        let oy=Math.abs(direction[1]);
-        let oz=Math.abs(direction[2]);
+    getNormal(point){
+        let ox=Math.abs(point[0]);
+        let oy=Math.abs(point[1]);
+        let oz=Math.abs(point[2]);
         if(ox>oy){
             if(ox>oz){
-                return [Math.sign(direction[0]), 0, 0];
+                return [Math.sign(point[0]), 0, 0];
             }else{
-                return [0, 0, Math.sign(direction[2])];
+                return [0, 0, Math.sign(point[2])];
             }
         }
         else{
             if(oy>oz){
-                return [0, Math.sign(direction[1]), 0];
+                return [0, Math.sign(point[1]), 0];
             }else{
-                return [0, 0, Math.sign(direction[2])];
+                return [0, 0, Math.sign(point[2])];
             }
         }
     }
