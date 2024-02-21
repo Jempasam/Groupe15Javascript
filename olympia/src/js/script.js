fd5a35b09675112163250d88066b5493ddae8fd1
Naylix <32761610+Naylix@users.noreply.github.com>
2024-02-21 16:33:39 +0100

    update GOW + lien vers snake

--- olympia/src/js/script.js
+++ olympia/src/js/script.js
@@ -93,9 +93,10 @@ function movePlayer(){
         case 1:
             camera.position.x = player.mesh.position.x;
             camera.position.z = player.mesh.position.z + 10;
+            break;
         default:
             camera.position.x = player.mesh.position.x;
-            camera.position.z = player.mesh.position.z+10;
+            camera.position.z = 10;
             break;
     }
     
