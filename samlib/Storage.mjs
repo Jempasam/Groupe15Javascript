f697009677d38256c1ef4b3e6e3976d710517c77
Jempasam <samueldemont83@gmail.com>
2024-03-12 10:43:09 +0100

    user storage

--- samlib/Storage.mjs
+++ samlib/Storage.mjs
@@ -1,3 +1,4 @@
+import { FlowGraphSendCustomEventBlock } from "babylonjs/FlowGraph/Blocks/Execution/flowGraphSendCustomEventBlock"
 
 
 /**
@@ -38,6 +39,13 @@ export const OBJECT_DATA ={
     parse: JSON.parse,
     serialize: JSON.stringify,
     get default(){return {}}
+}
+
+/** @type {StorageType<string>} */
+export const STRING_DATA ={
+    parse: e=>e,
+    serialize: e=>e,
+    get default(){return ""}
 }
 
 /** @type {StorageType<Array>} */
@@ -122,16 +130,158 @@ class Storage{
 }
 
 /**
+ * @typedef {{name:string,password:string,image:string,creation:number}} PlayerSettings
+ * @typedef {{settings:PlayerSettings,data:Object}} PlayerData
+ */
+class AccountStorage extends Storage{
+    
+    constructor(storage){
+        super(storage)
+    }
+    
+    /**
+     * Get the current user
+     */
+    get current_user(){
+        let data=super.get("current_user",STRING_DATA)
+        if(data=="")return undefined
+        else return data
+    }
+
+    /**
+     * Set the current user
+     */
+    set current_user(username){
+        let data=super.set("current_user",STRING_DATA,username)
+    }
+
+    /**
+     * @returns {PlayerData=}
+     */
+    #get_user_data(username){
+        let players=super.get("players",OBJECT_DATA)
+        if(!players)return undefined
+        let player=players[username]
+        if(!player)return undefined
+    }
+
+    /**
+     * Add a new player and return his data if it succeed
+     * @param {string} name 
+     * @param {string} password 
+     * @returns {PlayerSettings=}
+     */
+    add_player(name,password){
+        let already=this.#get_user_data(name)
+        if(!already){
+            let players=super.get("players",OBJECT_DATA)
+            players[name]={settings:{name, password, image:null, creation:Date.now()}, data:{}}
+            super.set("players",OBJECT_DATA,players)
+            return players[name].settings
+        }
+        return undefined
+    }
+
+    /**
+     * Remove player and return if it succeed
+     * @param {string} name 
+     * @returns {boolean} true if it succeed
+     */
+    remove_player(name){
+        let players=super.get("players",OBJECT_DATA)
+        if(players[name]){
+            delete players[name]
+            super.set("players",OBJECT_DATA,players)
+            return true
+        }
+        else return false
+    }
+
+    /**
+     * Get the player data of the given player if it exist
+     * @param {string} name 
+     * @returns {PlayerSettings=}
+     */
+    get_player(name){
+        return this.#get_user_data(name)?.settings
+    }
+
+    /**
+     * Set the settings of the given player
+     * @param {string} name 
+     * @param {string} data 
+     * @returns {boolean} true if it succeed
+     */
+    set_player(name,data){
+        let players=super.get("players",OBJECT_DATA)
+        if(players[name]){
+            players[name].settings=data
+            super.set("players",OBJECT_DATA,players)
+            return true
+        }
+        else return false
+    }
+
+    get players(){
+        return Object.keys(super.get("players",OBJECT_DATA))
+    }
+
+    /**
+     * Get the given player if the password is good
+     * @param {string} name 
+     * @param {string} password
+     * @returns {PlayerSettings=}
+     */
+    try_get_player(name,password){
+        const player=this.#get_user_data(name)
+        if(player && player.settings.password==password){
+            return player.settings
+        }
+    }
+
+    get(name,type){
+        let str=this.#get_user_data(this.current_user)?.data[name]
+        if(str==null)return type.default
+        else{
+            let data=type.parse(str)
+            if(data==null)return type.default
+            else return data
+        }
+    }
+    
+    set(name,type,data){
+        let str=type.serialize(data)
+        let storage=this.#get_user_data(this.current_user)?.data
+        if(str!=null && storage)storage[name]=str
+        else return data.default
+    }
+
+    remove(name){
+        let storage=this.#get_user_data(this.current_user)?.data
+        if(storage)delete storage[name]
+    }
+
+    /**
+     * Create a new storage accessor to the same storage location with his own prefix
+     */
+    clone(){
+        let storage=new AccountStorage(this.storage)
+        storage.current_user=this.current_user
+        storage.prefix=this.prefix
+        return storage
+    }
+
+}
+
+/**
  * A simple local storage, never cleared
  */
 export const LOCAL_STORAGE=new Storage(localStorage)
 
 /**
- * A copy of local storage, but with a different prefix.
- * Use this one for account-based storage.
- * Use prefix to set the account id.
+ * A local storage with accounts
  */
-export const ACCOUNT_STORAGE=LOCAL_STORAGE.clone()
+export const ACCOUNT_STORAGE=new AccountStorage(localStorage)
 
 /**
  * a simple session storage, cleared when the session ends
